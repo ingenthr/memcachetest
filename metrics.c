8249c4bb42338cab7b43290efb685408088b4818
Matt Ingenthron <ingenthr@cep.net>
2010-03-08 02:22:38 -0800

    Fixed metrics to be set correctly.

--- metrics.c
+++ metrics.c
@@ -194,8 +194,10 @@ static hrtime_t calc_average(struct TxnResult* a) {
  */
 void record_tx(enum TxnType tx_type, hrtime_t time) {
     struct TxnResult* new_txn = calloc(1, sizeof(struct TxnResult));
+    new_txn->respTime = time;
+    new_txn->left = new_txn->right = new_txn->next = NULL;
     pthread_mutex_lock(&lock);
-    insert(&txn_list_first, create_txn(new_txn));
+    insert(&txn_list_first, new_txn);
     pthread_mutex_unlock(&lock);
     txCntStdy[tx_type]++;
 }
